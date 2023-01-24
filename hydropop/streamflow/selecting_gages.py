eb9f6baaf63f4e6226cee14cb036c93975bef904
Jon Schwenk <jonschwenk@gmail.com>
2023-01-24 09:47:27 -0700

    gages

--- hydropop/streamflow/selecting_gages.py
+++ hydropop/streamflow/selecting_gages.py
@@ -8,6 +8,7 @@ import geopandas as gpd
 from VotE import config
 config.vote_db()
 from VotE.streamflow import export_streamflow as es
+import os
 
 
 path_bounding_box = r"X:\Research\CIMMID\Data\Hydropop Layers\Finals\toronto_coarse\roi.gpkg" # shapefile of ROI
@@ -15,9 +16,9 @@ bb = gpd.read_file(path_bounding_box)
 gage_params = {'within' : bb.geometry.values[0],
                'max_drainarea_km2' : 10000,
                'fraction_valid' : .9,
-               'vote_addressed' : True,
+               'vote_snapped' : True,
                'end_date': '2000-01-01',
-               'min_span_yrs' : 10
+               'min_span_years' : 10
               }
 gage_ids = es.gage_selector(gage_params)
 gages = es.get_gages(gage_ids)
@@ -30,9 +31,27 @@ gage_locs = gpd.GeoDataFrame(data=gages[keepkeys], geometry=gages['mapped_geom']
 basins = gpd.GeoDataFrame(data=gages[keepkeys], geometry=gages['basin_geom_vote'], crs=gages.crs)
 
 gage_locs['start_date'] = gage_locs['start_date'].astype(str)
-gage_locs['end_date'] = gage_locs['start_date'].astype(str)
+gage_locs['end_date'] = gage_locs['end_date'].astype(str)
 basins['start_date'] = basins['start_date'].astype(str)
-basins['end_date'] = basins['start_date'].astype(str)
+basins['end_date'] = basins['end_date'].astype(str)
+
+# gage_locs.to_file(r'X:\Research\CIMMID\Data\Hydropop Layers\Finals\na_10k\gage_selection\na_10k_gages.gpkg', driver='GPKG')
+# basins.to_file(r'X:\Research\CIMMID\Data\Hydropop Layers\Finals\na_10k\gage_selection\na_10k_basins.gpkg', driver='GPKG')
+
+# # Updating to fix end_date
+# current_gages = r"X:\Research\CIMMID\Data\Hydropop Layers\Finals\na_10k\gage_selection\trap_data_basins.gpkg"
+# current_gages = gpd.read_file(current_gages)
+# gages = es.get_gages(current_gages['id_gage'].values.tolist())
 
 gage_locs.to_file(r'X:\Research\CIMMID\Data\Watersheds\Toronto\initial_gages.gpkg', driver='GPKG')
 basins.to_file(r'X:\Research\CIMMID\Data\Watersheds\Toronto\initial_basins.gpkg', driver='GPKG')
+
+""" Download streamflow data """
+for id_gage in basins['id_gage'].values:
+    print(id_gage)
+    df = es.get_streamflow_timeseries([int(id_gage)], start_date='1981-01-01', expand=True, trim=True)
+    df.drop('id_gage', inplace=True, axis=1)
+    df.sort_values(by='date', inplace=True)
+    path_out = os.path.join(r'X:\Research\CIMMID\Data\Hydropop Layers\Finals\na_10k\streamflow', str(id_gage) + '.csv')
+    df.to_csv(path_out, index=False)
+
