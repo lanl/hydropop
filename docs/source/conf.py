db41ae059e95023c8973a2e9646b094e3e62d676
Jemma Stachelek <jemma.stachelek@gmail.com>
2024-12-18 10:15:51 -0700

    battle sphinx module discovery

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ release = "0.1"
 import os
 import sys
 import hydropop
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
